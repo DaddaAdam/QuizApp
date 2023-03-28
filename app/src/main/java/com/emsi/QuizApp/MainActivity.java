f363e48584e3ee8b0cc0fed44bcbd45709662fbc
DaddaAdam <daddaadam@gmail.com>
2023-03-28 22:53:45 +0000

    added firebase authentication

--- app/src/main/java/com/emsi/QuizApp/MainActivity.java
+++ app/src/main/java/com/emsi/QuizApp/MainActivity.java
@@ -1,11 +1,20 @@
 package com.emsi.QuizApp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +22,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
+
         setContentView(R.layout.activity_main);
+
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
 
         TextView email = (TextView) findViewById(R.id.email);
         TextView password = (TextView) findViewById(R.id.password);
@@ -23,7 +37,30 @@ public class MainActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(v -> {
             if(!email.getText().toString().isEmpty()
                     && !password.getText().toString().isEmpty()) {
-                Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL!", Toast.LENGTH_LONG).show();
+
+                String emailString = email.getText().toString();
+                String passwordString = password.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(emailString, passwordString)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(MainActivity.this, "Authentication succeeded.",
+                                            Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+
             }
         });
 
