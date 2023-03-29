8528e6732476d335590eba1bdfe49a6c0726bc61
DaddaAdam <daddaadam@gmail.com>
2023-03-29 21:41:46 +0000

    get multiple questions from firestore and loop through them

--- app/src/main/java/com/emsi/QuizApp/QuizActivity.java
+++ app/src/main/java/com/emsi/QuizApp/QuizActivity.java
@@ -1,71 +1,91 @@
 package com.emsi.QuizApp;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
 import com.squareup.picasso.Picasso;
 
-
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class QuizActivity extends AppCompatActivity {
+    //Global variables
+    private static int questionCount=0;
+    private static int score = 0;
 
-    private static int questionCount=1;
+    //UI elements
+    Button answer1, answer2, answer3, answer4;
+    TextView questionNumber, questionContent;
+    ImageView quiz_image;
 
+    //Firebase elements
+    FirebaseFirestore db;
+    List<DocumentSnapshot> documents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db = FirebaseFirestore.getInstance();
 
-        DocumentReference docRef = db.collection("Questions").document("q1");
-        docRef
+        questionNumber = (TextView) findViewById(R.id.questionNumber);
+        quiz_image = (ImageView) findViewById(R.id.quiz_image);
+        questionContent = (TextView) findViewById(R.id.questionContent);
+
+        answer1 = (Button) findViewById(R.id.answer_1);
+        answer2 = (Button) findViewById(R.id.answer_2);
+        answer3 = (Button) findViewById(R.id.answer_3);
+        answer4 = (Button) findViewById(R.id.answer_4);
+
+        db.collection("Questions")
                 .get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        Map<String, Object> question = documentSnapshot.getData();
-
-                        if(question != null){
-                            DisplayQuestion(question);
-                    }
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            documents = task.getResult().getDocuments();
+                            DisplayQuestion(documents.get(questionCount).getData());
+                        }
                     }
                 });
+
+        answer1.setOnClickListener(v -> {
+            ProcessQuestions(answer1.getText().toString());
+        });
+        answer2.setOnClickListener(v -> {
+            ProcessQuestions(answer2.getText().toString());
+        });
+        answer3.setOnClickListener(v -> {
+            ProcessQuestions(answer3.getText().toString());
+        });
+        answer4.setOnClickListener(v -> {
+            ProcessQuestions(answer4.getText().toString());
+        });
     }
 
     public void DisplayQuestion(Map<String, Object> question){
-        TextView questionNumber = (TextView) findViewById(R.id.questionNumber);
-        ImageView quiz_image = (ImageView) findViewById(R.id.quiz_image);
-        TextView questionContent = (TextView) findViewById(R.id.questionContent);
-
-        Button answer1 = (Button) findViewById(R.id.answer_1);
-        Button answer2 = (Button) findViewById(R.id.answer_2);
-        Button answer3 = (Button) findViewById(R.id.answer_3);
-        Button answer4 = (Button) findViewById(R.id.answer_4);
-
-        questionNumber.setText((String)"Question N°"+questionCount);
-        questionContent.setText((String)question.get("content"));
         ArrayList<String> answers =  (ArrayList<String>) question.get("answers");
+
+        questionNumber.setText((String)"Question N°"+questionCount+1);
+        questionContent.setText((String)question.get("content"));
+        
         answer1.setText(answers.get(0));
         answer2.setText(answers.get(1));
         answer3.setText(answers.get(2));
@@ -75,7 +95,17 @@ public class QuizActivity extends AppCompatActivity {
         Picasso.get().load(imageUrl).into(quiz_image);
     }
 
-    public void ProcessQuestions(){
+    public void ProcessQuestions(String answer){
+
+        if(answer.equals(documents.get(questionCount).getData().get("correct_answer")))
+            score++;
+
+        if(questionCount < documents.size())
+            DisplayQuestion(documents.get(++questionCount).getData());
+
+        else
+            Toast.makeText(QuizActivity.this, "No more questions.",
+                    Toast.LENGTH_SHORT).show();
 
     }
 
